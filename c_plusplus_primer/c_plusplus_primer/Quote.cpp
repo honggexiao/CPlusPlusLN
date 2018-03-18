eabf3c556014c5499d9dbb2d50a00781416f8686
honggexiao <honggexiao@sina.com>
2018-03-18 23:28:37 +0800

    [chap15] add class funtion

--- c_plusplus_primer/c_plusplus_primer/Quote.cpp
+++ c_plusplus_primer/c_plusplus_primer/Quote.cpp
@@ -10,6 +10,7 @@ using std::size_t;
 using std::ostream; using std::endl;
 using std::cout;
 
+Bulk_quote::Bulk_quote(const std::string &book, double p, std::size_t qty, double disc) : Quote(book, p), min_qty(qty), discount(disc) {}
 double Bulk_quote::net_price(std::size_t cnt) const
 {
 	if (cnt > min_qty)
--- c_plusplus_primer/c_plusplus_primer/Quote.h
+++ c_plusplus_primer/c_plusplus_primer/Quote.h
@@ -1,10 +1,9 @@
 #pragma once
-#include <string>
-#include <set>
-#include <iostream>
-#include <fstream>
-#include <sstream>
 #include <memory>
+#include <iostream>
+#include <string>
+#include <cstddef>
+#include <utility>
 
 class Quote {
 public:
@@ -17,4 +16,13 @@ private:
 	std::string bookNo;
 protected:
 	double price = 0.0;
+};
+class Bulk_quote : public Quote {
+public:
+	Bulk_quote() = default;
+	Bulk_quote(const std::string&, double, std::size_t, double);
+	double net_price(std::size_t) const override;
+private:
+	std::size_t min_qty = 0;
+	double discount = 0.0;
 };
